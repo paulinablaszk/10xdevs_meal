ace11a4df9d0b6697e001bc9a47b6e8978514073
Paulina Błaszkiewicz <paulina.blaszkiewicz@currency-one.com>
2025-07-29 11:31:54 +0200

    Add test user

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -5,4 +5,6 @@ import type { Database } from './database.types';
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
-export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey); 
+export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const DEFAULT_USER_ID = '77cba856-5259-4019-96c3-6044e8e993d3';
